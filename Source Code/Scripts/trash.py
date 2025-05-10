5555a5c8642a7b291181b2f7032ceb0a7ab78e53
Jo Jeong Hyeon <165864420+CTRLHyeon@users.noreply.github.com>
2025-05-10 21:07:33 +0900

    Update trash.py
    divided trash_generator function in to trash_generator() and trash_count_print

--- Source Code/Scripts/trash.py
+++ Source Code/Scripts/trash.py
@@ -1,8 +1,13 @@
 """
 TitlePage.py, Game_UI에 맞춰 제작한 trash.py 파일
-열린 땅에만 쓰레기 생성, 쓰레기 갯수 세기는 정상적으로 작동하나, 출력이 제대로 되지 않는 문제
 
-from trash import trash_generator, TRASH_TEXT <- game_UI에 작성해야함함
+[현재 구현 성공/실패 목록]
+쓰레기 생성(O)
+쓰레기 갯수 증가(O)
+쓰레기 출력(X)
+쓰레기 갯수 출력(O)
+
+game_UI 많이 편집해놔서 새로 커밋해야하는데 일단 game_UI는 커밋 안했음음
 """
 
 
@@ -11,12 +16,11 @@ import pygame
 import random
 pygame.init()
 #---------------Global Variable----------------------------
-trash_count =0
+
 #SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 TRASH_TEXT = pygame.font.Font("fonts/Jalnan.ttf", 38)
 
-def trash_generator(trash_gen_tick, screen, tiles, tile_map):
-    global trash_count
+def trash_generator(trash_gen_tick, screen, tiles, tile_map, trash_count):
     trash_gen_tick += 1
     if (trash_gen_tick >= 20):
         trash_gen_tick = 0
@@ -27,7 +31,9 @@ def trash_generator(trash_gen_tick, screen, tiles, tile_map):
             b = random.randint(0, (HEIGHT//TILE_SIZE)-1)
         screen.blit(tiles[3], (a*TILE_SIZE, b*TILE_SIZE))
         trash_count += 1
-        trash_count_text = TRASH_TEXT.render(str(trash_count), True, BLACK)
-        screen.blit(trash_count_text, (30, 400))
         
-    return trash_gen_tick
+    return trash_gen_tick, trash_count
+
+def trash_count_print(screen, trash_count):
+        trash_count_text = TRASH_TEXT.render(str(trash_count), True, BLACK)
+        screen.blit(trash_count_text, (1760, 545))
