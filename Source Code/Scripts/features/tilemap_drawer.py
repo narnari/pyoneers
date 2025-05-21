10d6859c5d834e93d78f473ac0fe5d94ee83caf1
ali-zarq1512 <zarqali@chungbuk.ac.kr>
2025-05-21 20:27:37 +0900

    Create tilemap_drawer.py
    uploaded a code of tile map drawer file.

---
+++ Source Code/Scripts/features/tilemap_drawer.py
@@ -0,0 +1,69 @@
+import pygame, sys, ctypes
+from pygame.locals import QUIT
+from Scripts.features import land_editor, trash_editor, tree_editor
+from Scripts.utils import assets, config
+ctypes.windll.user32.SetProcessDPIAware()
+"""
+tile_objects는 오브젝트 그리는거 전용으로 하는 배열
+1 : 쓰레기
+2 : 불
+3~6 : 나무 1~4
+"""
+tiles = {}
+tile_map = [[False for _ in range(config.GRID_WIDTH)] for _ in range(config.GRID_HEIGHT)]
+tick = 0
+tile_objects = [[0 for _ in range(config.GRID_WIDTH)] for _ in range(config.GRID_HEIGHT)]
+
+
+# --- 중앙 3x3 타일만 선명하게 설정 ---
+center_x = config.GRID_WIDTH // 2
+center_y = config.GRID_HEIGHT // 2
+for row in range(center_y, center_y + 3):
+    for col in range(center_x - 1, center_x + 2):
+        tile_map[row][col] = True
+
+initialized = None
+def load_assets():
+    global tiles, UIs, initialized
+    if not initialized:
+        tiles = {
+            "tile": assets.load_image("Tile04.png", (64, 64)),
+            "tile_tree1": assets.load_image("Tree1Plant.png",(64, 64)),
+            "tile_tree2": assets.load_image("Tree2Plant.png", (64, 64)),
+            "tile_tree3": assets.load_image("Tree3Plant.png", (64, 64)),
+            "tile_tree4": assets.load_image("Tree4Plant.png", (64, 64)),
+            "trash": assets.load_image("trash03.png", (64, 64)),
+        }
+
+def get_transparent_tile(tile, alpha):
+    copy_tile = tile.copy()
+    copy_tile.set_alpha(alpha)
+    return copy_tile
+
+
+def draw_tilemap(screen):
+    faded_tile = get_transparent_tile(tiles["tile"], 100)
+    for row in range(4, config.GRID_HEIGHT - 1):
+        for col in range(5, config.GRID_WIDTH - 5):
+            x = col * config.TILE_SIZE
+            y = row * config.TILE_SIZE
+            tile = tiles["tile"] if tile_map[row][col] else faded_tile
+            screen.blit(tile, (x, y))
+
+    global tick, trash_count
+    tick, trash_editor.trash_count = trash_editor.generate_trash(tick, tile_objects, tile_map, trash_editor.trash_count)
+
+def draw_tile_objects(screen, tile_objects, tiles):
+    for row in range(4, config.GRID_HEIGHT-1):
+        for col in range(5, config.GRID_WIDTH-5):
+            obj = tile_objects[row][col]
+            if obj == 0:
+                continue
+            x = col * config.TILE_SIZE
+            y = row * config.TILE_SIZE
+            if obj == 1:
+                screen.blit(tiles["trash"], (x, y))
+            elif obj == 2:
+                screen.blit(tiles["fire"], (x, y))
+            elif 3 <= obj <= 6:
+                screen.blit(tiles[f"tile_tree{obj - 2}"], (x, y))
