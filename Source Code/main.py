e5435dd66698d42483af047b475fa71e8ee1f4ed
AgreePark <109130239+AgreePark@users.noreply.github.com>
2025-05-24 02:20:36 +0900

    Update main.py - 설정 화면으로 가는 코드 추가

--- Source Code/main.py
+++ Source Code/main.py
@@ -5,11 +5,15 @@ import os
 # 경로 설정
 sys.path.append(os.path.join(os.path.dirname(__file__), "Scripts"))
 
-from Scripts.screens import game_screen, title_screen
-from Scripts.utils import config
+from Scripts.screens import game_screen, title_screen, setting_screen
+from Scripts.utils import config, assets
 
 def main():
     pygame.init()
+    
+    setting_screen.bgm.play(-1)    # 무한 반복
+    setting_screen.bgm.set_volume(0.5)
+    
     screen = pygame.display.set_mode((config.WIDTH, config.HEIGHT), pygame.SCALED, pygame.FULLSCREEN)
     pygame.display.set_caption("Terrabloom")
 
@@ -24,13 +28,29 @@ def main():
                 state = "game"
             elif result == "exit":
                 running = False
+            elif result == "setting":
+                setting_screen.back_state = "title"
+                state = "setting"
         elif state == "game":
             result = game_screen.run_game(screen)
             if result == "title":
                 state = "title"
             elif result == "exit":
                 running = False
-
+            elif result == "setting":
+                setting_screen.back_state = "game"
+                state = "setting"
+        elif state == "setting":
+            # 처음 설정 창 실행 시 기본 설정이 가장 먼저 보이게
+            setting_screen.setting = "basic"
+            result = setting_screen.run_setting(screen)
+            if result == "title":
+                state = "title"
+            elif result == "game":
+                state = "game"
+            elif result == "exit":
+                running = False
+        
         pygame.display.flip()
         clock.tick(60)
 
