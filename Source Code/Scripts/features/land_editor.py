80a144feadf64cb7f18bc305c97ea8b40005cec1
NaYeonLee <168258375+narnari@users.noreply.github.com>
2025-06-04 11:08:09 +0900

    Update land_editor.py - Add land purchase status text

--- Source Code/Scripts/features/land_editor.py
+++ Source Code/Scripts/features/land_editor.py
@@ -1,7 +1,9 @@
 # land_editor.py
 
 from Scripts.features import resource_manager
+from Scripts.screens import game_screen
 from Scripts.utils import assets, config
+import time
 
 # 전역 변수: 현재 땅 편집 모드인지 여부
 editing_mode = False
@@ -56,18 +58,49 @@ def open_tile(tile_map, mouse_pos, tile_size):
     # 2. 자원이 충분한지 확인
     if not resource_manager.can_spend(money=cost):
         print("땅 살 돈 없음!")
+        game_screen.State.show_no_money_text = True
+        game_screen.State.show_no_money_time = time.time()
+
         return
 
     # 1. 타일을 잠금 해제 
     tile_map[row][col] = 1
 
+    # 땅 구매 완료 출력용
+    game_screen.State.show_success_money_text = True
+    game_screen.State.show_success_money_time = time.time()
+
     # 2. resource_manager에 있는 잠금 해제된 땅의 개수를 증가
     resource_manager.land_count += 1
 
-    # 3. 콘솔에 메시지 출력 (디버깅 및 정보 제공용)
+    # 3. 콘솔에 메시지 출력 (디버깅용)
     print(f"땅 열었음! 비용 {cost}원 차감됨!")
 
     #toggle_mode() # 하나의 타일을 구매한 직후 편집 모드를 종료
+
+# 땅 열 돈 없음 화면에 출력
+def draw_no_money_text(screen):
+    global EDITING_TEXT
+
+    # pygame 초기화 이후에 폰트 로딩
+    if EDITING_TEXT is None:
+        EDITING_TEXT = assets.load_font("Jalnan.ttf", 38)
+
+        # text = EDITING_TEXT.render(f"땅 구매 비용 {resource_manager.can_spend(cost)}원!", True, config.BLACK)
+    text1 = EDITING_TEXT.render("돈 부족", True, config.BLACK)
+    text2 = EDITING_TEXT.render("확장 불가능!", True, config.BLACK)
+    screen.blit(text1, (100, 870))
+    screen.blit(text2, (50, 910)) 
+    return
+
+# 땅 구매 완료 화면에 출력
+def draw_success_text(screen):
+    global EDITING_TEXT
+    if EDITING_TEXT is None:
+        EDITING_TEXT = assets.load_font("Jalnan.ttf", 38)
+    text = EDITING_TEXT.render("땅 구매 완료!", True, config.BLACK)
+    screen.blit(text, (35, 870)) 
+
 
 # 땅 열린 모드 인지 화면에 그림
 def draw_editing_text(screen):
